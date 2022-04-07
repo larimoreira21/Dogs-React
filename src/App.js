fd0c7d352bab9cbbec6ac9f0c450df0d5b45815a
unknown <lari2102@gmail.com>
2022-04-07 16:15:21 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+// import Api from './api/api';
 import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
+import User from './Components/User/User';
+import ProtectedRouter from './Helper/ProtectedRouter';
+import Photo from './Components/Photo/Photo';
+import UserProfile from './Components/User/UserProfile';
+import NotFound from './Components/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+      <UserStorage>
+          <Header />
+          <main className='AppBody'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <Route path="conta/*" element={
+              <ProtectedRouter>
+                <User/>
+              </ProtectedRouter>}/>
+            <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          </main>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
     </div>
   );
 }
